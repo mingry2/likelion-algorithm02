4686303c0da20108a49a1b721d926ba467b43c5a
minkyoung kim <minry20415@naver.com>
2022-11-22 09:42:13 +0900

    [update] 재귀사용하여 1~100까지 숫자 출력(완료

--- src/main/java/algo1122/Print1To100V2.java
+++ src/main/java/algo1122/Print1To100V2.java
@@ -5,8 +5,11 @@ package algo1122;
 // 1~100까지 숫자 출력하기
 public class Print1To100V2 {
     public static void print1To100(int n){
+        if (n > 100) return; // 무한루프를 돌지 않게 탈출조건 달기
         System.out.println(n);
-        print1To100(n); // 자기 자신을 호출 -> 재귀 (StackOverflowError가 남 : 무한루프)
+        print1To100(n + 1);
+        // 자기 자신을 호출 -> 재귀 (StackOverflowError가 남 : 무한루프)
+        // n + 1을 해서 조건을 만들어줌
     }
     public static void main(String[] args) {
         print1To100(1);
